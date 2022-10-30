0c954b98fab606c57d9c2cf1b42d8edc76359e31
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-10-30 17:18:54 +0500

    connection and configuration of tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,vue}'],
+  build: {
+    postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    },
+  },
+  css: ['/src/assets/css/tailwind.css'],
+  theme: {
+    extend: {
+      colors: {
+        'black-app': '#150C21',
+        blackberry: '#1F1D2C',
+      },
+      fontFamily: {
+        inter: [
+          'Inter, ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont, Segoe UI,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji',
+        ],
+        poppins: [
+          'Poppins, ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont, Segoe UI,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji',
+        ],
+      },
+    },
+  },
+};
